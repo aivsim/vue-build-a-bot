6f409a6dda35dccbf9e0aafcaaafb7cf111cb1a9
Aivaras Šimulis <aivsim@amidus.lt>
2019-01-17 15:25:34 +0200

    Add routes

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,14 @@
       </nav>
     </header>
     <main>
-      <RobotBuilder />
+      <router-view/>
     </main>
   </div>
 </template>
 
 <script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
-
 export default {
   name: 'app',
-  components: {
-    RobotBuilder,
-  },
 };
 </script>
 
