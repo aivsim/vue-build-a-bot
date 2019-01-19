09a57a8396abd7015b59960206391ab86b349cbf
Aivaras Šimulis <aivaras.simulis@outlook.com>
2019-01-19 23:13:19 +0200

    Organize the store with modules

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -95,7 +95,7 @@ export default {
   mixins: [createdHookMixin],
   computed: {
     availableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.parts;
     },
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
@@ -116,7 +116,8 @@ export default {
         + robot.torso.cost
         + robot.rightArm.cost
         + robot.base.cost;
-      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }))
+        .then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
   },
