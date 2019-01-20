a52f63cb395e7c22665434f65909673cb12fd920
Aivaras Šimulis <aivaras.simulis@outlook.com>
2019-01-20 13:26:17 +0200

    Making directives available globally

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -21,8 +21,6 @@
 </template>
 
 <script>
-import pinDirective from '../shared/pin-directive';
-
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -35,7 +33,6 @@ function getNextValidIndex(index, length) {
 
 export default {
   name: 'PartSelector',
-  directives: { pin: pinDirective },
   props: {
     parts: {
       type: Array,
