7ea31a45d64dd4ab39c6a27fb66d3a8c83edfd43
Aivaras Šimulis <aivaras.simulis@outlook.com>
2019-01-20 12:21:33 +0200

    Using mapGetters helper

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
     <br/>
     Root Getter Foo: {{rootGetterFoo}} <br/>
     Robots Getter Foo: {{robotsGetterFoo}} <br/>
-    Users Getter Foo: {{usersGetterFoo}} <br/>
     <header>
       <nav>
         <ul>
@@ -43,7 +42,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   name: 'app',
@@ -53,15 +52,8 @@ export default {
       usersFoo: state => state.users.foo,
     }),
     ...mapState('robots', { robotsFoo: 'foo' }),
-    rootGetterFoo() {
-      return this.$store.getters.foo;
-    },
-    robotsGetterFoo() {
-      return this.$store.getters['robots/foo'];
-    },
-    usersGetterFoo() {
-      return this.$store.getters['users/foo'];
-    },
+    ...mapGetters({ rootGetterFoo: 'foo' }),
+    ...mapGetters('robots', { robotsGetterFoo: 'foo' }),
     cart() {
       return this.$store.state.robots.cart;
     },
